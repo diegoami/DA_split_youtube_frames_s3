2779293f8d7b759ca8ad6db7ff48b51405811f77
Diego Amicabile <diego.amicabile@gmail.com>
2019-10-27 21:51:33 +0100

    chore: extract frame splitting method

--- download_videos.py
+++ download_videos.py
@@ -43,6 +43,33 @@ def extract_videos_to_s3(s3_bucket, playlist_output, playlist_key):
             transfer.upload_file(full_file, s3_bucket, dest_file)
 
 
+def split_into_frames(frame_output_path, playlist_output, playlist_key):
+    files = os.listdir(playlist_output)
+    for file in files:
+        full_file = os.path.join(playlist_output, file)
+        vidcap = cv2.VideoCapture(full_file)
+        img_output = os.path.join(frame_output_path, playlist_key, file)
+        os.makedirs(img_output, exist_ok=True)
+
+        def getFrame(vidcap, sec, imgdir):
+            vidcap.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
+            hasFrames, image = vidcap.read()
+            if hasFrames:
+                cv2.imwrite(os.path.join(imgdir, "image" + str(count) + ".jpg"), image)  # save frame as JPG file
+            return hasFrames
+
+        sec = 0
+        frameRate = 5  # //it will capture image in each 0.5 second
+        count = 1
+
+        success = getFrame(vidcap, sec, img_output)
+        while success:
+            count = count + 1
+            sec = sec + frameRate
+            sec = round(sec, 2)
+            success = getFrame(vidcap, sec, img_output)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--playlist_id', type=str)
@@ -68,31 +95,7 @@ if __name__ == "__main__":
     playlist_key = playlist_id + '_' + sanitize_filename(playlist_name, restricted=True)
     playlist_output = os.path.join(video_output_path, playlist_key)
 
-    #download_videos(youtube_client=youtube_client, playlist_id=playlist_id, playlist_key=playlist_key, video_output_path=video_output_path)
-    #extract_videos_to_s3(s3_bucket=s3_bucket, playlist_output=playlist_output, playlist_key=playlist_key)
+    download_videos(youtube_client=youtube_client, playlist_id=playlist_id, playlist_key=playlist_key, video_output_path=video_output_path)
+    extract_videos_to_s3(s3_bucket=s3_bucket, playlist_output=playlist_output, playlist_key=playlist_key)
 
-    files = os.listdir(playlist_output)
-    for file in files:
-        full_file = os.path.join(playlist_output, file)
-        vidcap = cv2.VideoCapture(full_file)
-        img_output = os.path.join(frame_output_path, playlist_key, file)
-        os.makedirs(img_output, exist_ok=True)
-
-        def getFrame(vidcap, sec, imgdir):
-            vidcap.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
-            hasFrames, image = vidcap.read()
-            if hasFrames:
-                cv2.imwrite(os.path.join(imgdir, "image" + str(count) + ".jpg"), image)  # save frame as JPG file
-            return hasFrames
-
-
-        sec = 0
-        frameRate = 5  # //it will capture image in each 0.5 second
-        count = 1
-
-        success = getFrame(vidcap, sec, img_output)
-        while success:
-            count = count + 1
-            sec = sec + frameRate
-            sec = round(sec, 2)
-            success = getFrame(vidcap, sec, img_output)
+    split_into_frames(frame_output_path=frame_output_path, playlist_output=playlist_output, playlist_key=playlist_key)
