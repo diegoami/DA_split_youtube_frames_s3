e8e4d2f3ac8771baf4754c40c868df4e7c78ae86
Diego Amicabile <diego.amicabile@gmail.com>
2019-11-03 22:17:27 +0100

    Add support for ranges in playlist

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ The goal is to extract frames from videos in a playlist in youtube and save, say
 
 ## EXECUTION
 
-* `python main.py download_videos --playlist_id=<YOUR_PLAYLIST_ID>`
+* `python main.py download_videos --playlist_id=<YOUR_PLAYLIST_ID> --video_ranges=<comma separated list of ranges, e.g. 1-3,5-9>`
 * `python main.py upload_videos_to_s3 --playlist_id=<YOUR_PLAYLIST_ID>`
 * `python main.py split_into_frames --playlist_id=<YOUR_PLAYLIST_ID>`
 * `python main.py upload_frames_to_s3 --playlist_id=<YOUR_PLAYLIST_ID>`
--- split_youtube_frames/youtube_downloader.py
+++ split_youtube_frames/youtube_downloader.py
@@ -2,12 +2,27 @@ import logging
 import os
 from youtube_dl import YoutubeDL, DEFAULT_OUTTMPL
 
+def get_int_video_ranges(video_ranges):
+    videos_selection = []
+    video_ranges_l = video_ranges.split(',')
+    for vr in video_ranges_l:
+        if '-' in vr:
+            vr_l = vr.split('-')
+            videos_selection.extend([x for x in range(int(vr_l[0]), int(vr_l[1])+1)])
+        else:
+            videos_selection.append(int(vr))
+    return videos_selection
 
-def download_videos(youtube_client, playlist_id, playlist_key, video_output_path, video_format):
+def download_videos(youtube_client, playlist_id, playlist_key, video_output_path, video_format, video_ranges):
     playlist_output = os.path.join(video_output_path, playlist_key)
     logging.info("Downloading from playlist {} to {}".format(playlist_key, playlist_output))
-
+    if video_ranges:
+        video_sel = get_int_video_ranges(video_ranges)
+    else:
+        video_sel = None
+    index = 1
     for video_items in youtube_client.iterate_videos_in_playlist(playlist_id):
+        logging.info(f"Trying to Downloading {index+1} video")
         for item in video_items['items']:
             snippet = item['snippet']
             content_details = item['contentDetails']
@@ -16,8 +31,12 @@ def download_videos(youtube_client, playlist_id, playlist_key, video_output_path
             print(video_id, title)
             video_info = youtube_client.get_video(video_id)
             url = "http://www.youtube.com/watch?v={}".format(video_id)
-
-            with YoutubeDL({'format': str(video_format), 'merge-output-format': 'mp4', 'cachedir': False,
-                            'outtmpl': playlist_output + '/' + DEFAULT_OUTTMPL, "nooverwrites": True, "restrictfilenames": True}) as youtube_dl:
-                youtube_dl.download([url])
+            if not video_sel or index in video_sel:
+                logging.info(f"Downloading {index}th of playlist ")
+                with YoutubeDL({'format': str(video_format), 'merge-output-format': 'mp4', 'cachedir': False,
+                                'outtmpl': playlist_output + '/' + DEFAULT_OUTTMPL, "nooverwrites": True, "restrictfilenames": True}) as youtube_dl:
+                    youtube_dl.download([url])
+            else:
+                logging.info(f"Skipping {index}th of playlist ")
+            index += 1
     return playlist_output
