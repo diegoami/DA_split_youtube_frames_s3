6286da1130626f6067d763498b32e7a99e5df4cf
Diego Amicabile <diego.amicabile@gmail.com>
2019-11-09 00:05:27 +0100

    Download description file

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     # use dispatch pattern to invoke method with same name
 
     parser.add_argument('--playlist_id', type=str, required=True)
-    parser.add_argument('--video_ranges', type=str)
+    parser.add_argument('--video_range', type=str)
 
     with open('config.yml') as f:
         config = yaml.safe_load(f)
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     playlist_id = args.playlist_id
-    video_ranges = args.video_range
+    video_range = args.video_range
     youtube_client = YoutubeClient(config['client_json_file'])
     playlist_name = youtube_client.playlist_name(playlist_id)
 
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     frame_output = os.path.join(frame_output_path,  playlist_key)
 
     if args.command == 'download_videos':
-        download_videos(youtube_client=youtube_client, playlist_id=playlist_id, playlist_key=playlist_key,  video_output_path=video_output_path, video_format=config['video_format'], video_ranges=video_ranges)
+        download_videos(youtube_client=youtube_client, playlist_id=playlist_id, playlist_key=playlist_key,  video_output_path=video_output_path, video_format=config['video_format'], video_range=video_range)
     if args.command == 'upload_videos_to_s3':
         upload_videos_to_s3(s3_bucket=config['s3_bucket'], playlist_output=playlist_output, playlist_key=playlist_key)
 
--- split_youtube_frames/youtube_downloader.py
+++ split_youtube_frames/youtube_downloader.py
@@ -1,7 +1,9 @@
 import logging
 import os
 from youtube_dl import YoutubeDL, DEFAULT_OUTTMPL
+from youtube_dl.utils import sanitize_filename
 
+'%(title)s-%(id)s.%(ext)s'
 def get_int_video_ranges(video_ranges):
     videos_selection = []
     video_ranges_l = video_ranges.split(',')
@@ -13,11 +15,11 @@ def get_int_video_ranges(video_ranges):
             videos_selection.append(int(vr))
     return videos_selection
 
-def download_videos(youtube_client, playlist_id, playlist_key, video_output_path, video_format, video_ranges):
+def download_videos(youtube_client, playlist_id, playlist_key, video_output_path, video_format, video_range):
     playlist_output = os.path.join(video_output_path, playlist_key)
     logging.info("Downloading from playlist {} to {}".format(playlist_key, playlist_output))
-    if video_ranges:
-        video_sel = get_int_video_ranges(video_ranges)
+    if video_range:
+        video_sel = get_int_video_ranges(video_range)
     else:
         video_sel = None
     index = 1
@@ -27,15 +29,23 @@ def download_videos(youtube_client, playlist_id, playlist_key, video_output_path
             snippet = item['snippet']
             content_details = item['contentDetails']
             video_id = content_details['videoId']
-            title = snippet['title']
-            print(video_id, title)
-            video_info = youtube_client.get_video(video_id)
+            video_snippet = youtube_client.get_video_snippet(video_id)
+            description = video_snippet["description"]
+            title = video_snippet["title"]
+
+
             url = "http://www.youtube.com/watch?v={}".format(video_id)
+
             if not video_sel or index in video_sel:
-                logging.info(f"Downloading {index}th of playlist ")
+                logging.info(f"Downloading {index}th of playlist... ")
                 with YoutubeDL({'format': str(video_format), 'merge-output-format': 'mp4', 'cachedir': False,
                                 'outtmpl': playlist_output + '/' + DEFAULT_OUTTMPL, "nooverwrites": True, "restrictfilenames": True}) as youtube_dl:
                     youtube_dl.download([url])
+                logging.info(f"Done")
+                description_filename = sanitize_filename(f'{title}-{video_id}.dsc')
+                with open(os.path.join(playlist_output, description_filename), 'w') as dfw:
+                    dfw.writelines(description)
+                logging.info(f"Description saved to {description_filename}")
             else:
                 logging.info(f"Skipping {index}th of playlist ")
             index += 1
